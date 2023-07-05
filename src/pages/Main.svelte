9fa641f03704677fe6c6e83d06f5282d616be7b3
prgmr99 <johnyeom24@naver.com>
2023-07-05 23:09:25 +0900

    [Rename] Svelte로 이전의 코드 옮기기

---
+++ src/pages/Main.svelte
@@ -0,0 +1,61 @@
+<script>
+  let hour = new Date().getHours().toString().padStart(2, "0");
+  let min = new Date().getMinutes().toString().padStart(2, "0");
+</script>
+
+<header>
+  <div class="info-bar">
+    <div class="info-bar__time">{hour}:{min}</div>
+    <div class="info-bar__icons">
+      <img src="assets/svg/chart-bar.svg" alt="chart-bar" />
+      <img src="assets/svg/wifi.svg" alt="wifi" />
+      <img src="assets/svg/battery.svg" alt="battery" />
+    </div>
+  </div>
+  <div class="menu-bar">
+    <div class="menu-bar__location">
+      <div>역삼1동</div>
+      <img src="assets/svg/arrow-down.svg" alt="arrow-down" />
+    </div>
+    <div class="menu-bar__icons">
+      <img src="assets/svg/glass.svg" alt="glass" />
+      <img src="assets/svg/bar.svg" alt="bar" />
+      <img src="assets/svg/bell.svg" alt="bell" />
+    </div>
+  </div>
+</header>
+
+<main>
+  <a class="wrBtn" href="write.html">글쓰기</a>
+</main>
+
+<footer>
+  <div class="nav-bar">
+    <div class="footer-icons">
+      <img src="assets/svg/home.svg" alt="" />
+      <span class="footer-icon__text">홈</span>
+    </div>
+    <div class="footer-icons">
+      <img src="assets/svg/document.svg" alt="" />
+      <span class="footer-icon__text">동네현황</span>
+    </div>
+    <div class="footer-icons">
+      <img src="assets/svg/map-pin.svg" alt="" />
+      <span class="footer-icon__text">내 위치</span>
+    </div>
+    <div class="footer-icons">
+      <a id="footer-chat" href="./chat.html">
+        <img src="assets/svg/chat.svg" alt="" />
+        <span class="footer-icon__text">대화 목록</span>
+      </a>
+    </div>
+    <div class="footer-icons">
+      <img src="assets/svg/user.svg" alt="" />
+      <span class="footer-icon__text">내 정보</span>
+    </div>
+  </div>
+</footer>
+<div class="media-info-msg">화면 사이즈를 줄여주세요.</div>
+
+<style>
+</style>
