df63f8dbb6289db6dd075ca2d7245d3106f74492
prgmr99 <johnyeom24@naver.com>
2023-07-07 14:46:12 +0900

    [Etc] 보안키 관리

--- firebase.js
+++ firebase.js
@@ -2,19 +2,17 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
-
 // TODO: Replace the following with your app's Firebase project configuration
 // See: https://firebase.google.com/docs/web/learn-more#config-object
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD_zo1EAWtKHxqKPlryXcBVN29FxN7U9oI",
-  authDomain: "carrot-market-8ce6a.firebaseapp.com",
-  databaseURL:
-    "https://carrot-market-8ce6a-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "carrot-market-8ce6a",
-  storageBucket: "carrot-market-8ce6a.appspot.com",
-  messagingSenderId: "391315034849",
-  appId: "1:391315034849:web:3bc47d64e38d434e8a9da0",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_DATABASE_URL,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 // Initialize Firebase
